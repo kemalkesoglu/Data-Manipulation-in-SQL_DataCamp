ab0e851c188e46873fae54da2b67e85394d11f74
Kemal KEŞOĞLU <kemalkesoglu@gmail.com>
2025-04-18 00:04:13 +0300

    We'll take the CASE.sql

--- We'll take the CASE.sql
+++ We'll take the CASE.sql
@@ -59,3 +59,43 @@ WHERE
 	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
 		WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win' 
 		END IS NOT NULL;
+
+--COUNT using CASE WHEN
+SELECT 
+	c.name AS country,
+	COUNT(CASE WHEN m.season = '2012/2013' THEN m.id END) AS matches_2012_2013,
+	COUNT(CASE WHEN m.season = '2013/2014' THEN m.id END) AS matches_2013_2014,
+	COUNT(CASE WHEN m.season = '2014/2015' THEN m.id END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+--COUNT and CASE WHEN with multiple conditions
+SELECT 
+	c.name AS country,
+	SUM(CASE WHEN m.season = '2012/2013' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2012_2013,
+ 	SUM(CASE WHEN m.season = '2013/2014' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2013_2014,
+	SUM(CASE WHEN m.season = '2014/2015' AND m.home_goal > m.away_goal
+        THEN 1 ELSE 0 END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
+
+--Calculating percent with CASE and AVG
+SELECT 
+	c.name AS country,
+	ROUND(AVG(CASE WHEN m.season='2013/2014' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2013/2014' AND m.home_goal <> m.away_goal THEN 0
+			 END),2) AS pct_ties_2013_2014,
+	ROUND(AVG(CASE WHEN m.season='2014/2015' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2014/2015' AND m.home_goal <> m.away_goal THEN 0
+			 END),2) AS pct_ties_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
